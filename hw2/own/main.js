8d1fcef1be90292b442af1c4a59afc0ad66dcc47
Yuan <ilmlbnba@gmail.com>
2021-03-03 22:16:59 +0800

    disable the button displaying the first/last image

--- hw2/own/main.js
+++ hw2/own/main.js
@@ -11,7 +11,20 @@ display.src = img_source[current];
 a.href = img_source[current];
 a.innerHTML = img_source[current];
 
-var switch_img = function(a) {
+var checkBtn = function(a) {
+    if (a === 0) {
+        document.getElementById("previous").style.opacity = 0.5;
+    } else if (a < img_source.length - 1) {
+        let tmp = document.getElementsByClassName("image-viewer__button");
+        for (let i = 0 ; i < tmp.length; ++i) {
+            tmp[i].firstElementChild.style.opacity = 1;
+        }
+    } else {
+        document.getElementById("next").style.opacity = 0.5;
+    }
+}
+
+var switchImg = function(a) {
     let tmp = current + a;
     if (tmp < img_source.length && tmp >= 0) {
         current = tmp;
@@ -19,11 +32,13 @@ var switch_img = function(a) {
         a.href = img_source[current];
         a.innerHTML = img_source[current];
     }
+    checkBtn(current);
 }
 
+checkBtn(current);
 let btn = document.getElementsByClassName("image-viewer__button");
 for (let i = 0 ; i < btn.length; ++i) {
     let tmp = (btn[i].firstElementChild.id === "previous")? -1:1;
-    btn[i].addEventListener("click", function() {switch_img(tmp);}); 
+    btn[i].addEventListener("click", function() {switchImg(tmp);}); 
 }
 
