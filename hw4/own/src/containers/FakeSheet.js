8de95da9745cc5f190ec81224a4d59de19c5f02f
Yuan <ilmlbnba@gmail.com>
2021-04-06 03:08:50 +0800

    add button component

--- hw4/own/src/containers/FakeSheet.js
+++ hw4/own/src/containers/FakeSheet.js
@@ -1,11 +1,30 @@
 import React, { Component } from "react";
-import Header from "../components/Header";
+// import Header from "../components/Header";
 
 class FakeSheet extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
     render() {
         return (
             <>
-                <Header />
+                {/* <Header /> */}
+                <div class="row">
+                    <div class="change_column">
+                        <button id="add_column">+</button>
+                        <button id="erase_column">-</button>
+                    </div>
+                </div>
+                <div class="column">
+                    <div class="change_row">
+                        <button id="add_row">+</button>
+                        <button id="erase_row">-</button>
+                    </div>
+                </div>
+                
+                {/* <Sheet> */}
             </>
         );
     }
