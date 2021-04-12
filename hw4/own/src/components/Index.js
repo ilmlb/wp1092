b5f8385ff594c4f377c58bb9feb96858390744fc
Yuan <ilmlbnba@gmail.com>
2021-04-12 22:34:07 +0800

    bug on add row and column

--- hw4/own/src/components/Index.js
+++ hw4/own/src/components/Index.js
@@ -1,14 +1,16 @@
-import React, { useState }  from "react";
-export default ({action, dark}) => {
-    const [index, setIndex] = useState(100);
-
-    return <div class="index_box">{intToIndex(index, dark)}</div>
+import React from "react";
+export default ({len, dark}) => {
+    return <div class="index_box">{intToIndex(len, dark)}</div>;
 }
 
-function intToIndex(n) {
+function intToIndex(n, d) {
     let idx = [];
     for (let i = 0; i < n; ++i) {
-        idx.push(<div>{i + 1}</div>);
+        if (i != d) {
+            idx.push(<div>{i + 1}</div>);
+        } else {
+            idx.push(<div class="dark">{i + 1}</div>);
+        }
     }
     return idx;
 }
--- hw4/own/src/containers/Sheet.js
+++ hw4/own/src/containers/Sheet.js
@@ -1,12 +1,44 @@
-import React, { useState } from "react";
-import Row from "../components/Row";
+import React, { Component } from "react";
 
-export default ({action, pos}) => {
-    let s = [];
-    for (let i = 0; i < 100; ++i) {
-        s.push(<Row row_idx={i} focus={pos}/>);
+class Sheet extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            // content: this.initialSheet(),
+        };
     }
-    const [grid, setGrid] = useState(s); // save Rows in grid
 
-    return <div class="sheet">{grid}</div>;
+    setContent = (t, i, j) => {
+        let c = this.props.content;
+        c[i][j] = t;
+        this.setState({content: c});
+    }
+
+    render() {
+        let sheet = new Array(this.props.content.length);
+        for (let i = 0; i < this.props.content.length; ++i) {
+            let row = new Array(this.props.content[0].length);
+            for (let j = 0; j < this.props.content[0].length; ++j) {
+                row[j] = <input type="text"
+                                ref={input => this.ref = input}
+                                value={this.props.content[i][j]}
+                                onClick={(e) => {e.target.select()}}
+                                onKeyDown={(e) => {
+                                    if (e.key === "Enter") {
+                                        if (this.props.now[0] >= 0 && this.props.now[0] < this.props.content.length - 1) {
+                                            e.target.blur();
+                                            this.props.pos(i + 1, j);
+                                        }
+                                    }
+                                }}
+                                onChange={(e) => this.setContent(e.target.value.trim(), i, j)}
+                                onBlur={() => this.props.pos(-1,- 1)}
+                                onFocus={() => this.props.pos(i, j)}/>;
+            }
+            sheet[i] = <div class="row">{row}</div>;
+        }
+        return <div class="sheet">{sheet}</div>;
+    }
 }
+
+export default Sheet;
