2366d2777c4bba6aa779bec4c0971b5eebe0aedd
Yuan <ilmlbnba@gmail.com>
2021-06-15 12:01:12 +0800

    insertPeople done

--- hack4/backend/src/resolvers/Mutation.js
+++ hack4/backend/src/resolvers/Mutation.js
@@ -4,13 +4,16 @@ const Mutation = {
         for (let i = 0; i < args.data.length; ++i) {
             const person = db.people.find((person) => person.ssn === args.data[i].ssn);
             if (person) {
+                console.log("update")
                 person.name = args.data[i].name;
                 person.location = args.data[i].location;
                 person.severity = args.data[i].severity;
             } else {
+                console.log("insert")
                 db.people.push(args.data[i]);
             }
         }
+        // console.log(db.people);
         return true;
     }
 }
